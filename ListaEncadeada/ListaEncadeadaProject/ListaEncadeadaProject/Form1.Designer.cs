9c0a7647c1888d5b2f62bd1754e123dd0d447745
Matheus Oliveira <matolisou@gmail.com>
2016-02-25 10:12:09 -0300

    Version 0.2
    -Alpha
    -Um pouco de interface
    -Lógica de Element semi pronta

---
+++ ListaEncadeada/ListaEncadeadaProject/ListaEncadeadaProject/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace ListaEncadeadaProject
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Numero = new System.Windows.Forms.TextBox();
+            this.Proximo = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // Numero
+            // 
+            this.Numero.Location = new System.Drawing.Point(311, 236);
+            this.Numero.Name = "Numero";
+            this.Numero.Size = new System.Drawing.Size(100, 20);
+            this.Numero.TabIndex = 0;
+            // 
+            // Proximo
+            // 
+            this.Proximo.Location = new System.Drawing.Point(311, 283);
+            this.Proximo.Name = "Proximo";
+            this.Proximo.Size = new System.Drawing.Size(100, 20);
+            this.Proximo.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(311, 324);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Criar Novo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.AdicionaElemento);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(213, 44);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(296, 186);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(756, 385);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Proximo);
+            this.Controls.Add(this.Numero);
+            this.Name = "Form1";
+            this.Text = "Lista Encadeada";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Numero;
+        private System.Windows.Forms.TextBox Proximo;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+
+    }
+}
+
---
+++ ListaEncadeada/ListaEncadeadaProject/ListaEncadeadaProject/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ListaEncadeadaProject
+{
+    public partial class Form1 : Form
+    {
+        private Lista list;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InicializarLista();
+            ExibirLista();
+            Numero.Text = "0";
+        }
+
+        class Element
+        {
+            #region Element
+            public static int Nome;
+            public Element anterior;
+            public Element proximo;
+
+            public Element(int value)
+            {
+                ValueSelf = value;
+                proximo = null;
+                anterior = null;
+            }
+
+            public Element(int value, Element next = null)
+            {
+                ValueSelf = value;
+                proximo = next;
+                anterior = null;
+            }
+
+            public Element(int value, Element next = null, Element previous = null) 
+            {
+                ValueSelf = value;
+                proximo = next;
+                anterior = previous;
+            }
+
+            public int ValueSelf
+            {
+                get
+                {
+                    return Nome;
+                }
+                set
+                {
+                    Nome = value;
+                }
+            }
+            public Element NextSelf
+            {
+                get
+                {
+                    return proximo;
+                }
+                set
+                {
+                    proximo = value;
+                }
+            }
+
+            public Element PreviousSelf
+            {
+                get
+                {
+                    return anterior;
+                }
+                set
+                {
+                    anterior = value;
+                }
+            }
+            #endregion
+        }
+
+        class Lista
+        {
+            #region Lista
+            private Element primeiro;
+            public Element Primeiro
+            {
+                get
+                {
+                    return primeiro;
+                }
+                set
+                {
+                    primeiro = value;
+                }
+            }
+
+            public Element Ultimo
+            {
+                get
+                {
+                    return BuscaUltimo();
+                }
+            }
+
+            public Lista()
+            {
+            
+            }
+
+            public void Adiciona(Element e)
+            {
+                if (Primeiro == null)
+                {
+                    Primeiro = e;
+                }
+                else
+                {
+                    Ultimo.proximo = e;
+                }
+            }
+
+            public Element BuscaUltimo()
+            {
+                Element current = Primeiro;
+                while (current.proximo != null)
+                {
+                    current = current.proximo;
+                }
+                return current;
+            }
+
+            public int Count
+            {
+                get 
+                {
+                    int count = 1;
+                    Element current = Primeiro;
+                    while (current != null)
+                    {
+                        current = current.proximo;
+                        count++;
+                    }
+                    return count;
+                }
+            }
+
+            public void ImprimeLista()
+            {
+                Element current = Primeiro;
+                while (current != null)
+                {
+                    current = current.proximo;
+                }
+            }
+            #endregion
+        }
+
+        //private void CarregarPrograma(object sender, EventArgs e)
+        //{
+        //    list = new Lista();
+        //}
+
+        private void InicializarLista()
+        {
+            list = new Lista();
+            Element NewElement = new Element(list.Count);            
+            list.Adiciona(NewElement);
+        }
+
+        private void AdicionaElemento(object sender, EventArgs e)
+        {
+            InicializarLista();
+            list = new Lista();
+            Element NewElement = new Element(list.Count);
+            list.Adiciona(NewElement);
+            listBox1.Items.Add("Ordem na lista : " + NewElement + "Numero : " + Numero.Text);
+            Numero.Clear();
+            Numero.Text = "0";
+        }
+
+        private void ExibirLista()
+        {
+            list.ImprimeLista();
+        }
+    }
+}
